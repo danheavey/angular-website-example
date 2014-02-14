444b4e499121eef34ce6b1b179feab9bd7680082
Dan Heavey <dan.heavey@gmail.com>
2014-02-14 22:32:51 +0000

    conetn and css updates

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,40 +6,45 @@ angular.module('myApp.controllers', [])
 
     .controller('HomeCtrl', ['$scope',
     function($scope) {
+        $scope.appTitle = "Generic Company";
         $scope.title = "Home";
         $scope.headerImage = "img/homepage.jpg";
     }])
+
     .controller('AboutUsCtrl', ['$scope',
     function($scope) {
         $scope.title = "About Us";
         $scope.headerImage = "img/default-page.jpg";
     }])
+
     .controller('ProductsCtrl', ['$scope',
      function($scope) {
         $scope.title = "Products";
         $scope.headerImage = "img/default-page.jpg";
-     }])
+    }])
+
     .controller('BrochureRequestCtrl', ['$scope',
      function($scope) {
         $scope.title = "Brochure Request";
          $scope.headerImage = "img/default-page.jpg";
-     }])
+    }])
+
     .controller('DownloadsCtrl', ['$scope',
      function($scope) {
          $scope.headerImage = "img/default-page.jpg";
          $scope.title = "Downloads";
-     }])
+    }])
+
     .controller('StockistCtrl', ['$scope',
     function($scope) {
         $scope.title = "Stockist";
         $scope.headerImage = "img/default-page.jpg";
-     }]);
+     }])
 
-
-
-function HeaderController($scope, $location)
-{
-    $scope.isActive = function (viewLocation) {
-        return viewLocation === $location.path();
-    };
-}
+    .controller('MenuController',['$scope','$location',
+    function ($scope, $location)
+    {
+        $scope.isActive = function (viewLocation) {
+            return viewLocation === $location.path();
+        };
+    }]);
