a9989b6655ecdf802532218021e1d908d3c13aa0
Dan Heavey <dan.heavey@gmail.com>
2014-02-14 19:14:34 +0000

    added page images and resized them

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,27 +6,33 @@ angular.module('myApp.controllers', [])
 
     .controller('HomeCtrl', ['$scope',
     function($scope) {
-        $scope.title = "Home"
+        $scope.title = "Home";
+        $scope.headerImage = "img/homepage.jpg";
     }])
     .controller('AboutUsCtrl', ['$scope',
     function($scope) {
-        $scope.title = "About Us"
+        $scope.title = "About Us";
+        $scope.headerImage = "img/default-page.jpg";
     }])
     .controller('ProductsCtrl', ['$scope',
-        function($scope) {
-            $scope.title = "Products"
+     function($scope) {
+        $scope.title = "Products";
+        $scope.headerImage = "img/default-page.jpg";
      }])
     .controller('BrochureRequestCtrl', ['$scope',
-        function($scope) {
-            $scope.title = "Brochure Request"
+     function($scope) {
+        $scope.title = "Brochure Request";
+         $scope.headerImage = "img/default-page.jpg";
      }])
     .controller('DownloadsCtrl', ['$scope',
-        function($scope) {
-            $scope.title = "Downloads"
+     function($scope) {
+         $scope.headerImage = "img/default-page.jpg";
+         $scope.title = "Downloads";
      }])
     .controller('StockistCtrl', ['$scope',
-        function($scope) {
-            $scope.title = "Stockist"
+    function($scope) {
+        $scope.title = "Stockist";
+        $scope.headerImage = "img/default-page.jpg";
      }]);
 
 
