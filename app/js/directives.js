ec080bad7837cb365d49a061fd4e7362252d1e42
Dan Heavey <dan.heavey@gmail.com>
2014-04-01 20:23:18 +0100

    clean up of js formatting

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,28 +1,30 @@
-'use strict';
+(function () {
+    'use strict';
 
-/* Directives */
+    /* Directives */
 
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+    angular.module('myApp.directives', [])
+        .directive('appVersion', ['version', function (version) {
+            return function (scope, elm, attrs) {
+                elm.text(version);
+            };
+        }]);
 
 // fix for ng-animate breaking carousel
+    angular.module('ui.bootstrap.setNgAnimate', ['ngAnimate'])
+        .directive('setNgAnimate', ['$animate', function ($animate) {
+            return {
+                link: function ($scope, $element, $attrs) {
 
-angular.module('ui.bootstrap.setNgAnimate', ['ngAnimate'])
-    .directive('setNgAnimate', ['$animate', function ($animate) {
-        return {
-            link: function ($scope, $element, $attrs) {
-
-                $scope.$watch( function() {
-                    return $scope.$eval($attrs.setNgAnimate, $scope);
-                }, function(valnew, valold){
-                    $animate.enabled(!!valnew, $element);
-                });
+                    $scope.$watch(function () {
+                        return $scope.$eval($attrs.setNgAnimate, $scope);
+                    }, function (valnew, valold) {
+                        $animate.enabled(!!valnew, $element);
+                    });
 
 
-            }
-        };
-    }]);
+                }
+            };
+        }]);
+
+}());
